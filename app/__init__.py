40937689ba8ea7ede205393a4dfffdfff2dfff83
Adam Szokalski <szokalskiadam@gmail.com>
2023-01-06 23:04:23 +0100

    fix __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,5 @@ from .utils import get_uuid
 from .players import Player, AIPlayer
 from .boards import Board, PlayerBoard
 from .game import Game
-from .ships import Ship, Submarine, Battleship, Destroyer, PetrolBoat, Carrier
+from .ships import Ship, Submarine, Battleship, Destroyer, PatrolBoat, Carrier
 from .config import BOARD_SIZE
